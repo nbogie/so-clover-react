d7672c0c41206be8dc32d0e73543710fcd365e8f
nbogie <neillbogie@googlemail.com>
2022-12-22 00:59:10 +0000

    start so-clover layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {CloverView()}
     </div>
   )
 }
 
 export default App
+
+function CloverView() {
+  return <div className={"oneClover"}>
+    <OneCardView words={["one", "two", "three", "four"]} rotation={0} />
+    <OneCardView words={["one", "two", "three", "four"]} rotation={3} />
+    <OneCardView words={["one", "two", "three", "four"]} rotation={2} />
+    <OneCardView words={["one", "two", "three", "four"]} rotation={1} />
+  </div>
+}
+
+type Rotation = 0 | 1 | 2 | 3;
+interface OneCardViewProps {
+  words: string[];
+  rotation: Rotation;
+}
+function OneCardView(props: OneCardViewProps): JSX.Element {
+  return (
+    <div className="oneCard">
+      {props.words.map(w => <div key={w}>{w}</div>)}
+    </div>
+  )
+}
